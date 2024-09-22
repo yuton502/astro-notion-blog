cab3b4a5c89a7e4951216a58fb2f7dee251741eb
yuton502 <yuton502.forver@gmail.com>
2024-09-22 15:18:06 +0900

    サイトマップの修正

--- src/lib/generate-sitemap.ts
+++ src/lib/generate-sitemap.ts
@@ -20,6 +20,9 @@ export function generateSitemap(items: SitemapItem[]) {
 
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${import.meta.env.SITE}</loc>
+     </url>
      ${itemsStr}
    </urlset>
  `
